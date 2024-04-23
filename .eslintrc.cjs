d605965adfb52a3aa539bc5b984e374cad48cf17
toris <ironjustlikethat@gmail.com>
2024-04-23 23:50:24 +0900

    ➕ Chore: eslint, prettier, tailwindcss 의존성 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
@@ -14,5 +18,11 @@ module.exports = {
     ],
     'react/prop-types': 'off',
     'prettier/prettier': ['error'],
+    'react/function-component-definition': [
+      // 함수선언 arrow function, function
+      2,
+      { namedComponents: ['arrow-function', 'function-declaration'] },
+    ],
+    'react/react-in-jsx-scope': 'off', // 17버전 이후로는 React import 필요 x
   },
 };
