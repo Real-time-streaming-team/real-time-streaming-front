d42f689ecae1065c0bde910b19574ff82619e293
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-23 22:32:09 +0900

    styled component 적용 컴포넌트 주석처리

--- src/components/Streaming/StreamerButton.jsx
+++ src/components/Streaming/StreamerButton.jsx
@@ -1,46 +1,73 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+// import styled, { css } from 'styled-components';
 
-const Button = styled.button`
-    padding: 0.5rem 1.25rem;
-    border-radius: 0.125rem;
-    align-items: center;
-    display: flex;
+// const Button = styled.button`
+//   padding: 0.5rem 1.25rem;
+//   border-radius: 0.125rem;
+//   align-items: center;
+//   display: flex;
 
-    ${props => props.subscribed ? css`
-        border: 1.5px solid transparent;
-        border-image-source: linear-gradient(180deg, #4ABEFF 0%, #001AFF 100%);
-        border-image-slice: 1;
-    ` : css`
-        background: linear-gradient(153.5deg, #4ABEFF -63.68%, #001AFF 75.59%);
-        transition: 1s;
-        &:hover {
-        background: linear-gradient(153.5deg, #2c94d0 -63.68%, #000f97 75.59%);
-        color: #bbbdca; /* 글자 색 변경 */
-    }
-    `}
-`;
+//   ${props =>
+//     props.subscribed
+//       ? css`
+//           border: 1.5px solid transparent;
+//           border-image-source: linear-gradient(
+//             180deg,
+//             #4abeff 0%,
+//             #001aff 100%
+//           );
+//           border-image-slice: 1;
+//         `
+//       : css`
+//           background: linear-gradient(
+//             153.5deg,
+//             #4abeff -63.68%,
+//             #001aff 75.59%
+//           );
+//           transition: 1s;
+//           &:hover {
+//             background: linear-gradient(
+//               153.5deg,
+//               #2c94d0 -63.68%,
+//               #000f97 75.59%
+//             );
+//             color: #bbbdca; /* 글자 색 변경 */
+//           }
+//         `}
+// `;
 
-const ButtonText = styled.div`
-    display: flex;
-    ${props => props.subscribed && css`
-        background: linear-gradient(120deg, #4ABEFF 0%, #001AFF 100%);
-        -webkit-background-clip: text;
-        background-clip: text;
-        color: transparent;
-        font-weight: bold;
-    `}
-`;
+// const ButtonText = styled.div`
+//   display: flex;
+//   ${props =>
+//     props.subscribed &&
+//     css`
+//       background: linear-gradient(120deg, #4abeff 0%, #001aff 100%);
+//       -webkit-background-clip: text;
+//       background-clip: text;
+//       color: transparent;
+//       font-weight: bold;
+//     `}
+// `;
+
+// const StreamerButton = ({ subscribed, followHandler }) => {
+//   return (
+//     <Button onClick={followHandler} subscribed={subscribed}>
+//       <ButtonText subscribed={subscribed}>
+//         <div className="mr-1">+</div>
+//         <div className="">Follow</div>
+//       </ButtonText>
+//     </Button>
+//   );
+// };
 
 const StreamerButton = ({ subscribed, followHandler }) => {
-    return (
-        <Button onClick={followHandler} subscribed={subscribed}>
-            <ButtonText subscribed={subscribed}>
-                <div className='mr-1'>+</div>
-                <div className=''>Follow</div>
-            </ButtonText>
-        </Button>
-    )
-}
+  return (
+    <button onClick={followHandler} subscribed={subscribed}>
+      {/* <ButtonText subscribed={subscribed}>
+        <div className="mr-1">+</div>
+        <div className="">Follow</div>
+      </ButtonText> */}
+    </button>
+  );
+};
 
-export default StreamerButton
+export default StreamerButton;
