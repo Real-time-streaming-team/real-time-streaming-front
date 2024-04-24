ce2dab8280c2cf25d67b6b57ee241d281cec17b0
toris <ironjustlikethat@gmail.com>
2024-04-24 22:18:03 +0900

    ✨ Feat: HLS video 추가

--- src/components/Streaming/StreamViewer.jsx
+++ src/components/Streaming/StreamViewer.jsx
@@ -1,68 +1,67 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Hls from 'hls.js';
 import { useEffect, useRef, useState } from 'react';
 
 const StreamViewer = () => {
-  const videoRef = useRef(null); // 비디오 요소를 위한 ref 생성
-  const [mute, setMute] = useState(false);
-
+  const [isMuted, setIsMuted] = useState(false);
+  const videoRef = useRef(null);
+  const hlsRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
   useEffect(() => {
-    const getMedia = async () => {
-      try {
-        // 사용자의 웹캠으로부터 미디어 스트림을 얻습니다
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-          audio: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream; // 스트림을 비디오 요소의 srcObject 속성에 할당
-        }
-      } catch (error) {
-        console.error('Error accessing media devices.', error);
-      }
-    };
-
-    getMedia();
-    console.log(videoRef.current, videoRef.current.srcObject);
-
+    if (Hls.isSupported()) {
+      const video = videoRef.current;
+      hlsRef.current = new Hls();
+      hlsRef.current.loadSource(
+        `${import.meta.env.VITE_STREAM_ADDRESS}/1.m3u8`,
+      );
+      hlsRef.current.attachMedia(video);
+    }
     return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        // 컴포넌트 언마운트 시 스트림 정리
-        const tracks = videoRef.current.srcObject.getTracks();
-        tracks.forEach(track => track.stop());
-        videoRef.current.srcObject = null;
-      }
+      // 컴포넌트 언마운트 시 hls.js 인스턴스 해제
+      hlsRef.current.destroy();
     };
-  }, []);
-
-  const pauseHandler = () => {
-    const videoTracks = videoRef.current.srcObject.getVideoTracks();
-    console.log(videoTracks);
+  }, [hlsRef]);
+  const toggleMute = () => {
+    const video = videoRef.current;
+    video.muted = !isMuted; // 현재 상태의 반대값으로 설정
+    setIsMuted(!isMuted); // 상태 업데이트
   };
-
-  const muteHandler = () => {
-    const audioTracks = videoRef.current.srcObject.getAudioTracks();
-    audioTracks.forEach(item => {
-      if (mute) item.enabled = false;
-      else item.enabled = true;
-    });
-    setMute(prev => !prev);
+  // 재생 및 일시정지 토글 함수
+  const togglePlay = () => {
+    const video = videoRef.current;
+    console.log(video.duration);
+    if (isPlaying) {
+      video.pause();
+      video.currentTime = video.duration - 3; // 가장 최신으로 이동
+    } else {
+      video.play();
+    }
+    setIsPlaying(!isPlaying); // 상태 업데이트
   };
-
   return (
     <div className="relative flex-1 overflow-hidden">
       {/* video */}
       <div className="h-full bg-black">
-        <video className="m-auto size-full" ref={videoRef} autoPlay />
+        <video className="m-auto size-full" ref={videoRef} autoPlay controls>
+          <track
+            src={`${import.meta.env.VITE_STREAM_ADDRESS}/1.m3u8`}
+            kind="captions"
+            srcLang="ko"
+            label="korea_captions"
+          />
+        </video>
       </div>
 
       {/* controller */}
-      <div className="bg-mute absolute bottom-0 left-0 w-full opacity-0 transition-all duration-500 hover:opacity-100 ">
+      <div className="absolute bottom-0 left-0 w-full opacity-0 transition-all duration-500 hover:opacity-100 ">
         <div className="flex justify-between">
           <div className="flex">
-            <button onClick={pauseHandler}>멈춤</button>
-            <button onClick={muteHandler}>{mute ? 'unmute' : 'mute'}</button>
-          </div>
-          <div className="flex">
-            <button>시간</button>
+            <button type="button" onClick={toggleMute}>
+              {isMuted ? 'Unmute' : 'Mute'}
+            </button>
+            <button type="button" onClick={togglePlay}>
+              {isPlaying ? 'Pause' : 'Play'}
+            </button>
           </div>
         </div>
       </div>
