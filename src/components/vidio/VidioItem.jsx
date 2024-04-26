62e4c92fa85a8f20c84dafefb1db045640baf0ef
toris <ironjustlikethat@gmail.com>
2024-04-26 12:36:00 +0900

    ✨ Feat: Homepage 추가

---
+++ src/components/vidio/VidioItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const VidioItem = ({ title, people, streamerId, userId, nickname }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      role="button"
+      className="flex cursor-pointer flex-col hover:bg-slate-700"
+      onClick={() => navigate(`/streaming/${streamerId}`)}
+      onKeyDown={e => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          navigate(`/streaming/${streamerId}`);
+        }
+      }}
+      tabIndex={0}
+    >
+      <img src="/streamImg.png" alt="썸네일" className="h-[195px] w-full" />
+      <div className="flex flex-col px-2 py-1">
+        <span className=" text-start text-white">{title}</span>
+        <div className="flex items-center justify-start">
+          <p className="text-slate-300">{nickname}</p>
+          <li className="ml-3 h-5 w-10 list-none rounded-md bg-red-600 text-center text-sm text-white">
+            <b>Live</b>
+          </li>
+        </div>
+      </div>
+      <div className="flex items-center px-2 py-1 text-sm text-white">
+        <img src="/icon-user.png" alt="iconUser" className="size-5" />
+        <b className="ml-1">{people}</b>
+      </div>
+    </div>
+  );
+};
+
+export default VidioItem;
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,5 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import dummyData from '../../components/vidio/dummyData.json';
+import VidioItem from '../../components/vidio/VidioItem';
+
+// 무한스크롤 구현예정
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const [streams, setStreams] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { streamList } = await axios.get(`/api/stream/list?page=1`);
+      setStreams([...dummyData]);
+      console.log(streamList);
+    })();
+    return () => {
+      setStreams([]);
+    };
+  }, []);
+  return (
+    <div className="flex">
+      <div className="mt-6 grid size-full grid-cols-5 gap-4">
+        {streams?.length !== 0 ? (
+          streams.map(stream => (
+            <VidioItem
+              key={stream.streamId}
+              title={stream.title}
+              people={stream.people}
+              streamerId={stream.streamerId}
+              userId={stream.streamer.userId}
+              nickname={stream.streamer.nickname}
+            />
+          ))
+        ) : (
+          <div className="flex w-full items-center justify-center">
+            <h2 className="text-2xl">방송하는 스트리머가 없습니다.</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
