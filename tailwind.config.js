c60dc8581a50aaf003cd9fccf333f5c8d5d59d47
shgusgh12 <https://github.com/shgusgh12>
2024-05-09 20:41:47 +0900

    Design: 폰트 및 아이콘 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans : ['Poppins'],
+    },
     extend: {
       colors:{
         'primary-color': '#4ABEFF',
         'secondary-color' : '#001AFF',
         'background' : '#0E0B19'
-      }
+      },
     },
   },
   plugins: [],
